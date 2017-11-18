bc37c3d0813af4349ab7b7a3a461870f976f3639
Bruno Soruco <bruno.soruco@gmail.com>
2017-11-18 16:41:56 -0300

    vibracion

--- app/src/main/java/com/soruco/bruno/tesisv1/MainActivity.java
+++ app/src/main/java/com/soruco/bruno/tesisv1/MainActivity.java
@@ -5,7 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
-import android.widget.Toast;
+import android.view.MotionEvent;
+import android.widget.Button;
 import android.widget.ToggleButton;
 import android.hardware.camera2.CameraAccessException;
 import android.view.View;
@@ -14,6 +15,7 @@ import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.os.Vibrator;
 import android.content.Context;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -64,12 +66,30 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
-
     }
 
     public void clickVibrate(View view) {
-        ((Vibrator)getSystemService(VIBRATOR_SERVICE)).vibrate(1000);
+        // Get instance of Vibrator from current Context
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        // Start without a delay
+        // Vibrate for 100 milliseconds
+        // Sleep for 1000 milliseconds
+        long[] pattern = {0, 100, 1000};
+
+        // The '0' here means to repeat indefinitely
+        // '0' is actually the index at which the pattern keeps repeating from (the start)
+        // To repeat the pattern from any other point, you could increase the index, e.g. '1'
+        v.vibrate(pattern, 0);
+        //((Vibrator)getSystemService(VIBRATOR_SERVICE)).vibrate(10000);
     }
+
+    public void clickStopVibrate(View view) {
+        // Get instance of Vibrator from current Context
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        v.cancel();
+    }
+
 
     public void clickSound(View view) {
         Uri notificationSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
